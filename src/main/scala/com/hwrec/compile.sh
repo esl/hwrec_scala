c3283bb86cf0f5a54c42dfa4fc7ab8433472fe16
Kacper Mentel <mentel.kk@gmail.com>
2020-02-12 16:16:14 +0100

    fixup! Adjust compile.sh to work with ubuntu

--- src/main/scala/com/hwrec/compile.sh
+++ src/main/scala/com/hwrec/compile.sh
@@ -2,5 +2,5 @@ g++ -shared -O3 \
     -I/usr/include \
     -I$JAVA_HOME/include \
     -I$JAVA_HOME/include/linux \
-    com_hwrec_JavaNativeCalculator.cpp -o libJavaNativeCalculator.dylib
-cp libJavaNativeCalculator.dylib ../../../../../
+    com_hwrec_JavaNativeCalculator.cpp -o libJavaNativeCalculator.so
+cp libJavaNativeCalculator.so ../../../../../
