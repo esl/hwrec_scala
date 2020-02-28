a970abf1bf9c0aa992458b2207f2c36ff572a6c0
Kacper Mentel <mentel.kk@gmail.com>
2020-02-28 14:39:08 +0100

    Repeat IO 1000x times

--- src/main/scala/com/hwrec/Routes.scala
+++ src/main/scala/com/hwrec/Routes.scala
@@ -37,7 +37,9 @@ trait Routes {
                 val resultFuture = recognize(inputData, k)
                 onSuccess(resultFuture) { digit =>
                   log.info("Recognized: {}", digit)
-                  println(inputJson)
+                  for (w <- 0 to 1000) {
+                    println(inputJson)
+                  }
                   val response = HwrecResponse(true, digit)
                   complete((StatusCodes.OK, response))
                 }
